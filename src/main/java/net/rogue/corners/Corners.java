6ee702c040a8379417b0a1ad9cb7a113e0de01a1
Jay <humanape329@gmail.com>
2024-05-28 14:17:31 -0400

    Initial Commit for Production branch

--- src/main/java/net/rogue/corners/Corners.java
+++ src/main/java/net/rogue/corners/Corners.java
@@ -1,10 +1,15 @@
 package net.rogue.corners;
 
+import lombok.*;
 import org.bukkit.plugin.java.*;
 
 public class Corners extends JavaPlugin {
 
+    @Getter private static Corners instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
     }
 }
